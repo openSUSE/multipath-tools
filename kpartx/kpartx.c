openSUSE/multipath-tools

commit 8c39e60cba6c6ce6f9153f3d219574fbdb8e6ea0

kpartx: fix some memory leaks
These leaks were reported by LeakSanitizer.

Signed-off-by: Martin Wilck <mwilck@suse.com>
Reviewed-by: Benjamin Marzinski <bmarzins@redhat.com>

kpartx/kpartx.c
@@ -227,6 +227,11 @@ xmalloc (size_t size) {
 	return t;
 }
 
+static void cleanup_charp(char **p)
+{
+	free(*p);
+}
+
 int
 main(int argc, char **argv){
 	int i, j, m, n, op, off, arg, c, d, ro=0;
@@ -237,10 +242,10 @@ main(int argc, char **argv){
 	char *type, *diskdevice, *device, *progname;
 	int verbose = 0;
 	char partname[PARTNAME_SIZE], params[PARTNAME_SIZE + 16];
-	char * loopdev = NULL;
-	char * delim = NULL;
-	char *uuid = NULL;
-	char *mapname = NULL;
+	char *loopdev __attribute__((cleanup(cleanup_charp))) = NULL;
+	char *delim __attribute__((cleanup(cleanup_charp))) = NULL;
+	char *uuid __attribute__((cleanup(cleanup_charp))) = NULL;
+	char *mapname __attribute__((cleanup(cleanup_charp))) = NULL;
 	int hotplug = 0;
 	int loopcreated = 0;
 	struct stat buf;
@@ -292,7 +297,9 @@ main(int argc, char **argv){
 			verbose = 1;
 			break;
 		case 'p':
-			delim = optarg;
+			delim = strdup(optarg);
+			if (!delim)
+				exit(1);
 			break;
 		case 'l':
 			what = LIST;
@@ -674,7 +681,6 @@ main(int argc, char **argv){
 		if (verbose)
 			fprintf(stderr, "loop deleted : %s\n", device);
 	}
-
 end:
 	dm_lib_exit();
 

commit 54ee121349f075d1fff2d9df354ec64ea2678913

kpartx: avoid double-free of mapname
The variable mapname is sometimes allocated and sometimes not.
To avoid double-free and still free allocated memory cleanly,
introduce a helper pointer for storing possibly allocated memory.

Fixes: 8c39e60 ("kpartx: fix some memory leaks")
Signed-off-by: Martin Wilck <mwilck@suse.com>

kpartx/kpartx.c
@@ -245,7 +245,8 @@ main(int argc, char **argv){
 	char *loopdev __attribute__((cleanup(cleanup_charp))) = NULL;
 	char *delim __attribute__((cleanup(cleanup_charp))) = NULL;
 	char *uuid __attribute__((cleanup(cleanup_charp))) = NULL;
-	char *mapname __attribute__((cleanup(cleanup_charp))) = NULL;
+	char *_mapname __attribute__((cleanup(cleanup_charp))) = NULL;
+	char *mapname;
 	int hotplug = 0;
 	int loopcreated = 0;
 	struct stat buf;
@@ -388,10 +389,14 @@ main(int argc, char **argv){
 	off = find_devname_offset(device);
 
 	if (!loopdev) {
-		mapname = dm_mapname(major(buf.st_rdev), minor(buf.st_rdev));
-		if (mapname)
-			uuid = dm_mapuuid(mapname);
+		_mapname = dm_mapname(major(buf.st_rdev), minor(buf.st_rdev));
+		if (_mapname)
+			uuid = dm_mapuuid(_mapname);
 	}
+
+	mapname = _mapname;
+	if (!mapname)
+		mapname = device + off;
 
 	/*
 	 * We are called for a non-DM device.
@@ -401,9 +406,6 @@ main(int argc, char **argv){
 	 */
 	if (!uuid && !(what == DELETE && force_devmap))
 		uuid = nondm_create_uuid(buf.st_rdev);
-
-	if (!mapname)
-		mapname = device + off;
 
 	if (delim == NULL) {
 		delim = xmalloc(DELIM_SIZE);