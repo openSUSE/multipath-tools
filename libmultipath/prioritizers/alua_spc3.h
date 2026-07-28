5c057d6ce30eb6a4545777a5b52b4771c48171d5
Benjamin Marzinski <bmarzins@redhat.com>
2026-07-27 23:05:45 -0400

    libmultipath: check for buffer overruns in get_asymmetric_access_state
    When get_asymmetric_access_state() calls do_rtpg(), it may return less
    than buflen bytes. RTPG_FOR_EACH_PORT_GROUP() only checked that the next
    port group descriptor started before the end of the buffer. Instead,
    get_asymmetric_access_state() should check that there is enough space
    for the entire port group descriptor in the data that was actually
    returned by do_rtpg(). Since struct rtpg_tpg_dscr has a variable size,
    it must first check if the base structure fits. Once it knows
    that it's safe to access port_count, it can check if the port data
    also fits.
    
    Fixes: a3cbee5 ("[lib] merge libprio in libmultipath")
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
    Reviewed-by: Martin Wilck <mwilck@suse.com>

--- libmultipath/prioritizers/alua_spc3.h
+++ libmultipath/prioritizers/alua_spc3.h
@@ -12,8 +12,8 @@
  *
  * This file is released under the GPL.
  */
-#ifndef __SPC3_H__
-#define __SPC3_H__
+#ifndef ALUA_SPC3_H_INCLUDED
+#define ALUA_SPC3_H_INCLUDED
 
 /*=============================================================================
  * Definitions to support the standard inquiry command as defined in SPC-3.
@@ -281,15 +281,4 @@ struct rtpg_data {
 	struct rtpg_tpg_dscr		data[0];
 } __attribute__((packed));
 
-#define RTPG_FOR_EACH_PORT_GROUP(p, g) \
-		for( \
-			g = &(p->data[0]); \
-			(((char *) g) - ((char *) p)) < get_unaligned_be32(p->length); \
-			g = (struct rtpg_tpg_dscr *) ( \
-				((char *) g) + \
-				sizeof(struct rtpg_tpg_dscr) + \
-				g->port_count * sizeof(struct rtpg_tp_dscr) \
-			) \
-		)
-
-#endif /* __SPC3_H__ */
+#endif /* ALUA_SPC3_H_INCLUDED */
