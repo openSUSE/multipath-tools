0af5f0e20adbab973622847d40d7a7cfe68d54a8
wallycheng <295412260@qq.com>
2026-08-14 11:22:54 +0800

    libmultipath: check get_word() result in disassemble_map()
    get_word() returns 0 and sets *word to NULL not only when calloc()
    fails, but also when the input table is malformed(e.g, the last path
    has no arguments). In both cases, dereferencing word later in
    atoi(word) causes a NULL pointer dereference.
    
    Add a check for !word and jump to out since word is already NULL,
    matching the error-handling pattern used elsewhere in the function.
    
    Fixes: https://github.com/opensvc/multipath-tools/issues/156
    Signed-off-by: wallycheng <295412260@qq.com>
    Reviewed-by: Benjamin Marzinski <bmarzins@redhat.com>
    Reviewed-by: Martin Wilck <mwilck@suse.com>

--- libmultipath/dmparser.c
+++ libmultipath/dmparser.c
@@ -392,6 +392,8 @@ int disassemble_map(vector pathvec, char *params, struct multipath *mpp,
 			for (k = 0; k < num_paths_args; k++)
 				if (k == 0) {
 					p += get_word(p, &word);
+					if (!word)
+						goto out;
 					def_minio = atoi(word);
 					FREE(word);
 
