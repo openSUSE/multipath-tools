61506b9309d9368ab5aa7f4bab759387b26c8ba3
Martin Wilck <mwilck@suse.com>
2026-08-27 16:28:46 +0000

    libmultipath: bump version to 0.14.5

--- libmultipath/version.h
+++ libmultipath/version.h
@@ -11,9 +11,9 @@
 #ifndef VERSION_H_INCLUDED
 #define VERSION_H_INCLUDED
 
-#define VERSION_CODE 0x000E04
+#define VERSION_CODE 0x000E05
 /* MMDDYY, in hex */
-#define DATE_CODE    0x070A1A
+#define DATE_CODE    0x081B1A
 
 #define PROG    "multipath-tools"
 
